27d9e3366e864badd7d3f15b5186898f94364fa4
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-06 18:27:21 -0400

    Complete exercise 6 of JavaScrip foundations
    Practiced using loops by making a function that copies a string
    repeatedly.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
 
+    let repeatedStr = "";
+    for (num; num > 0; num--) {
+        repeatedStr = repeatedStr + str;
+    }
+    return repeatedStr;
 };
 
 // Do not edit below this line
