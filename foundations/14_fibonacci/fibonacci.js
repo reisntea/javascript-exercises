b2ac3e6cfc2cf7e362f419b2b8fa21d3d494864a
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-09 21:20:55 -0400

    Complete exercise 14 of JavaScript foundations
    Practiced for loops by making a function that does returns a value from
    the fibonacci sequence.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // Since the sequence starts as 0, 1, 1, 2, 3, 5, etc.
+    let precedingNum = 0;
+    let currentNum = 1;
+
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+
+    // Each iteration sets currentNum to be the sum of the previous two values, and then sets the previous two values up a step in the sequence.
+    for (let i = 2; i <= num; i++) {
+        let nextNum = currentNum + precedingNum;
+        precedingNum = currentNum;
+        currentNum = nextNum;
+    }
+    return currentNum;
 
 };
 
