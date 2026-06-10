c2c54b752cb91ea122c879125fedf84df577eb68
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-09 20:37:16 -0400

    Complete exercise 13 of JavaScript exercises.
    Practiced arrays by turning a string into an array and back into a
    string.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // String containing all the valid characters.
+    const acceptedCharacters = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+    // Sets string to lowercase, then into an array of each character, then filters the array to only accept it if the character is in acceptedCharacters, and then puts it back into a string.
+    const cleanString = string
+        .toLowerCase()
+        .split('')
+        .filter((character) => acceptedCharacters.includes(character))
+        .join('');
+    
+    reversedString = cleanString.split('').reverse().join('');
+    return cleanString === reversedString;
 };
 
 // Do not edit below this line
