fc0eba58520b07f1f4073ccb97c6827a35de181e
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-09 20:05:59 -0400

    Complete exercise 12 of JavaScript foundations
    A little bit of array practice with calculator.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+  for (let i = num; i > 1; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
