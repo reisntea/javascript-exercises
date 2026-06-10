0d0aa0e5f42a9f09844db58a95ed9421c7be1835
Brian C <283823263+reisntea@users.noreply.github.com>
2026-06-09 22:06:00 -0400

    Complete exercise 16 of JavaScript foundations
    This was the actual 16th exercise, I accidentally said the wrong one
    on the last commit.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const oldest = array.sort((firstPerson, secondPerson) => {
+        if (!("yearOfDeath" in firstPerson)) firstPerson.yearOfDeath = (new Date()).getFullYear();
+        if (!("yearOfDeath" in secondPerson)) secondPerson.yearOfDeath = (new Date()).getFullYear();
 
+        let firstPersonAge = firstPerson.yearOfDeath - firstPerson.yearOfBirth;
+        let secondPersonAge = secondPerson.yearOfDeath - secondPerson.yearOfBirth;
+        return firstPersonAge > secondPersonAge ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
